b3e30a38ef6b35a8ac36d4c25dba95531edddcad
SergioGBosco <sergiogibosco@gmail.com>
2025-07-01 17:48:11 +0200

    {add] Section  Sconto in funzione dell'eta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,5 +8,25 @@ const età = parseInt(prompt("Inserisci la tua età"));
 
 // Costo del biglietto
 
-const price = 0.21 * percorso
-console.log(`il costro del tuo biglietto è ${price}€`);
+const price = 0.21
+
+// Sconto
+
+const under = 0.80
+
+const over = 0.60
+
+if (età < 18) {
+  pricerun = [(price * under) * percorso];
+}
+
+else if (età > 65) {
+  pricerun = [(price * over) * percorso];
+}
+
+else {
+  pricerun = price * percorso
+
+}
+
+console.log(`il costro del tuo biglietto è ${pricerun}€`);
