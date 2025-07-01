c46bcf9be4e6170af2fefa458d959196f1ab0265
SergioGBosco <sergiogibosco@gmail.com>
2025-07-01 17:14:14 +0200

    [Add] Section price

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,12 @@
 // Km da percorrere
 
-const percorso = prompt("Quanti Kilometri devi Percorrere?");
+const percorso = parseInt(prompt("Quanti Kilometri devi Percorrere?"));
 
 // età del passggero  
 
 const età = parseInt(prompt("Inserisci la tua età"));
 
+// Costo del biglietto
+
+const price = 0.21 * percorso
+console.log(`il costro del tuo biglietto è ${price}€`);
