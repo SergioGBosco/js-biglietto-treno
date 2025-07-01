12be886cf920a811298d2532b28628a092c74b30
SergioGBosco <sergiogibosco@gmail.com>
2025-07-01 18:41:16 +0200

    [Add] Method 2 to apply discount

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,13 +12,13 @@ const price = 0.21
 
 // Sconto
 
-const under = 0.80
+//const under = 0.80
 
-const over = 0.60
+//const over = 0.60
 
 // Condizione Metodo 1
 
-if (età < 18) {
+/*if (età < 18) {
   pricerun = [(price * under) * percorso];
 }
 
@@ -30,6 +30,26 @@ else {
   pricerun = price * percorso;
 
 }
+
+*/
+
+const under = 20
+
+const over = 40
+
+if (età < 18) {
+  pricerun = [(price * percorso) - [(price * percorso) * under / 100]]
+}
+
+else if (età > 65) {
+  pricerun = [(price * percorso) - [(price * percorso) * over / 100]]
+}
+
+else {
+  pricerun = price * percorso;
+}
+
+
 let finalpricerunarrotondato = Number(pricerun);
 let finalpricerun = finalpricerunarrotondato.toFixed(2);
 console.log(`il costo del tuo biglietto è ${finalpricerun}€`);
